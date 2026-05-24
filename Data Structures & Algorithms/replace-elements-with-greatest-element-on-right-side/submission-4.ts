cccd7a91003c4183b653caa0c4e9c65b89f273a6
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2026-05-24 15:05:44 -0300

    Add: replace-elements-with-greatest-element-on-right-side - submission-4

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-4.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr: number[]): number[] {
+        let greatest = -1;
+
+        for (let i = arr.length - 1; i >= 0; i--) {
+            const newCandidate = arr[i];
+            arr[i] = greatest;
+            greatest = Math.max(greatest, newCandidate);
+        }
+
+        return arr;
+    }
+}
