33c98f76039400f72002effcb0dfe4f8c0a5d790
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2026-05-24 15:18:45 -0300

    Add: reverse-a-linked-list - submission-9

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-9.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    reverseList(head: ListNode | null): ListNode {
+        let curr = head;
+        let prev = null;
+
+        while (curr) {
+            const nextIteration = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextIteration;
+        }
+
+        return prev;
+    }
+}
